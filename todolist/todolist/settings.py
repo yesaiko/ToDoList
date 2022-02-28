dedc9716de3b3d71ea22351186682106fafc353a
Sylwia Matula <xxxyesaiko@gmail.com>
2022-02-28 21:19:24 +0100

    feat: connect to database

--- todolist/todolist/settings.py
+++ todolist/todolist/settings.py
@@ -73,10 +73,15 @@ WSGI_APPLICATION = 'todolist.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'todolist',
+        'USER': 'postgres',
+        'PASSWORD': 'docker',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
